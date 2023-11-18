da96b19b9b1b1e7cedd1394ea3dd8f91ddb35958
Mariano <marinocapo2006@gmail.com>
2023-11-17 23:19:16 -0300

    solved

--- aschat.js
+++ aschat.js
@@ -23,7 +23,8 @@ const inputArea = document.getElementById("input");
 let chatHistorial = [];
 const chatFragment = document.createDocumentFragment();
 const chatHistorialContainer = document.getElementById("chatHistorial");
-const API_KEY = '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'
+const API_KEY = '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'
+let data;
 //const asData = "MUCHO MÁS QUE EDUCACIÓNEstimulamos al niño desde la primera infancia para formar ciudadanos responsables y socialmente comprometidos, a través de propuestas motivadoras y desafiantes y con el apoyo de un cuerpo docente innovador y motivador que inspira al alumno a “aprender a aprender”, a convivir, a hacer y a ser: hoy, mañana y siempre.Nuestro equipo realiza un seguimiento detallado de los procesos cognitivos y emocionales de cada alumno. Apostamos a la constante comunicación favoreciendo la comprensión entre padres, docentes y alumnos. La familia juega un rol fundamental para acompañar el objetivo en común de cuidar el rendimiento y el bienestar del niño.Todo el trayecto educativo y formativo de tus hijos en una misma institución, con los mismos objetivos. Maternal, primaria, secundaria, inglés como segunda lengua y deportes.F. Fondar 843 Trinidad, FloresAsistimos a un acuerdo unánime en relación a la relevancia de la Educación Inicial, es una realidad por nuestros niños/as, por su educación, por su crecimiento y desarrollo en una visión de país que brega por una formación humana de las nuevas generaciones.El requerimiento de la Educación Inicial está sustentado en los principios de que el tramo etario (0 a 6 años) “es una etapa crucial en la vida de las personas, en la que se producen los procesos más importantes del desarrollo y requieren de ambientes enriquecidos afectiva y culturalmente para favorecer aprendizajes oportunos en situaciones motivantes”(del Marco Curricular…Dic2014"
 
 let input = ""
@@ -76,8 +77,8 @@ function delay(ms) {
 async function callGPT(inputText){
     await delay(1000)
     let writingItem = document.createElement("P");
-        writingItem.textContent = "escribiendo"
-        writingItem.classList.add("writing__item")
+        writingItem.textContent = "escribiendo";
+        writingItem.classList.add("writing__item");
         chatHistorialContainer.appendChild(writingItem);
     
 
@@ -94,19 +95,19 @@ async function callGPT(inputText){
                 'content': /*asData +"eres un asistente virtual de un colegio. responde todas las dudas y se muy amable" +*/ inputText,
             }]
         })
-    })} catch(error){
+    })
+    data = await res.json();
+    console.log(data);
+    } catch(error){
         console.error('Error en la llamada a la API:', error);
     }finally{
-        chatHistorial.lastChild.remove();
+        chatHistorialContainer.lastChild.remove();
+        addMessageItem(data.choices[0].message.content.toString(), 'gpt');
     }
     
-
     
-
-    const data = await res.json();
-    console.log(data);
     
-    addMessageItem(data.choices[0].message.content.toString(), 'gpt');
+    
     
 }
 
